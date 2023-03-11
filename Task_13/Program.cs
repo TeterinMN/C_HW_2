14687b65e1a4b29730beac12e145a4b653ef3d34
TeterinMN <Scorpion.TMN@ya.ru>
2023-03-11 22:44:10 +0900

    Написал код к задаче 13

---
+++ Task_13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num > 999)
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    Console.WriteLine(num % 10);
+}
+else if (num < 99)
+    Console.WriteLine("Третьей цифры нет ");
+else
+    Console.WriteLine(num % 10);
